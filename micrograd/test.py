0b4980bdb92a72320daa87d309e7c41f0cfcc96f
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-24 14:44:39 -0300

    Testing my mlp implematation on a artificial dataset

--- micrograd/test.py
+++ micrograd/test.py
@@ -1,39 +1,33 @@
-import torch 
-from micrograd.engine import Value
+from micrograd.nn import MLP
+import torch
+n = MLP(3,[4,4,1])
 
-#My implementation 
-n1 = Value(2.0)
-w1 = Value(0.0)
-n2 = Value(-3.0)
-w2 = Value(1.0)
-b = Value(6.8)
-x = n1*w1 + n2*w2 +b
-o = x.tanh()
-o.backaward()
-print(o.data)
-print("-------")
-print("Gradiants")
-print(f"n1={n1.grad}")
-print(f"w1={w1.grad}")
-print(f"n2={n2.grad}")
-print(f"w2={w2.grad}")
-print("-------")
+xs = [
+  [2.0, 3.0, -1.0],
+  [3.0, -1.0, 0.5],
+  [0.5, 1.0, 1.0],
+  [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0]
 
 
-#Pytorch
-n1 = torch.Tensor([2.0]).double();   n1.requires_grad = True
-w1 = torch.Tensor([0.0]).double();   w1.requires_grad = True
-n2 = torch.Tensor([-3.0]).double();   n2.requires_grad = True
-w2 = torch.Tensor([1.0]).double();   w2.requires_grad = True
-b = torch.Tensor([6.8]).double();   b.requires_grad = True
-x = n1*w1 + n2*w2 + b
-o = torch.tanh(x)
-o.backward()
-print(o.data.item())
-print("-------")
-print("Gradiants")
-print(f"n1={n1.grad.item()}")
-print(f"w1={w1.grad.item()}")
-print(f"n2={n2.grad.item()}")
-print(f"w2={w2.grad.item()}")
-print("-------")
+for k in range(20):
+    ys_predict = [n(x) for x in xs]
+    loss =0
+    for yr,yf in zip(ys,ys_predict):
+        loss = (yf-yr)**2+loss
+
+    for p in n.parameters():
+        p.grad=0
+    loss.backaward()
+
+    for p in n.parameters():
+        p.data += -0.05*p.grad
+    print(k,loss)
+
+
+ys_predict = [n(x) for x in xs]
+print(ys_predict)
+
+
+
