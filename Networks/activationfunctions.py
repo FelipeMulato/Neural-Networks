4a97dbd6580be426684b095ee6992dde9de96f8d
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-03 15:11:28 -0300

    Adding Sigmoid

--- Networks/activationfunctions.py
+++ Networks/activationfunctions.py
@@ -3,3 +3,7 @@ import numpy as np
 class Relu():
     def forward(self, Inputs):
         return np.maximum(0,Inputs)
+    
+class Sigmoid():
+    def forward(self, Inputs):
+        return np.power((1-np.exp(-1*Inputs)),-1)
